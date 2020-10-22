8858962b3295fc81af5f318397a8d9880742648f
XZRC <oskar.hasslefalk@gmail.com>
2020-10-22 13:47:06 +0200

    Renamed shit

--- src/main/java/fail/mercury/client/Mercury.java
+++ src/main/java/fail/mercury/client/Mercury.java
@@ -22,7 +22,7 @@ public class Mercury {
 
     public static Mercury INSTANCE = new Mercury();
 
-    public static final String NAME = "Elementars+";
+    public static final String NAME = "FrogWare";
     public static final String VERSION = "1.0";
 
     private final PropertyManager propertyManager = new PropertyManager();
@@ -47,7 +47,7 @@ public class Mercury {
 
     private File directory = new File(Util.mc.mcDataDir, NAME);
 
-    public static final Logger log = LogManager.getLogger("Mercury");
+    public static final Logger log = LogManager.getLogger("FrogWare");
 
     public void init() {
         try {
